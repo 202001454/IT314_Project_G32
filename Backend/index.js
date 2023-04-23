aa8442b5162f50533d458275431477003c260b1e
202001454 <202001454@daiict.ac.in>
2023-04-23 22:18:21 +0530

    JWT authentication and authorization done

--- Backend/index.js
+++ Backend/index.js
@@ -1,35 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const { json } = require('express');
 const methodoverride = require('method-override');
 const path = require('path');
 const dbURI = 'mongodb+srv://gaurang:gaurang@cluster0.olbixf6.mongodb.net/dummy?retryWrites=true&w=majority';
 const authRoutes = require('./routes/authRoutes');
 
-async function validatepassword(password) {
-    if (password.length < 6 || password.length > 16) {
-        return false;
-    }
-    // Number check
-    if (!password.match(/[0-9]+/)) {
-        return false;
-    }
-    // Uppercase check
-    if (!password.match(/[A-Z]+/)) {
-        return false;
-    }
-    // Lowercase check
-    if (!password.match(/[a-z]+/)) {
-        return false;
-    }
-    // Special character check
-    if (!password.match(/[!@#$%^&*()]+/)) {
-        return false;
-    }
-    return true;
-
-}
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => console.log('Connected to DB'))
@@ -47,8 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(static_path));
 app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(authRoutes);
-
 
 
 
