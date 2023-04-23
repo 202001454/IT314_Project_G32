f95ebbbf27ede7931aeb10dac9b53b8e1f3dfb91
GaurangParmar1810 <202001444@daiict.ac.in>
2023-04-23 10:26:03 +0530

    function.js created and initiated!

---
+++ Backend/functions.js
@@ -0,0 +1,10 @@
+const User = require('../models/user');
+const Payment = require('../models/payment');
+const Paymenthistory = require('../models/paymenthistory');
+const Feedback = require('../models/feedback');
+
+
+const mamnager_view_get = async (req,res) => {
+    const {username} = req.params;
+    
+}
