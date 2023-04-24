5a9536968b4a848dfd4388cb2f8ed63d0154f2c7
Vrund87 <vrundrajput2002@gmail.com>
2023-04-24 16:45:20 +0530

    added cadet_about request

--- Backend/method.js
+++ Backend/method.js
@@ -4,8 +4,25 @@ const cadet_get = async (req, res) => {
         const cadet = await User.findOne({ username: username });
         console.log(cadet);
         res.render('cadet/index', { cadet: cadet });
-    } catch (error) {   
+    } catch (error) {
         console.log(error);
         res.send('An error occurred while finding the cadet.');
     }
 }
+
+const cadet_about_get = async (req, res) => {
+    try {
+        const username = req.params.username;
+        const cadet = await User.findOne({ username: username, role: 'cadet' });
+        if (cadet) {
+            res.render('cadet/about', { cadet: cadet });
+        }
+        else {
+            res.send('An error occurred while finding the cadet.');
+        }
+    } catch (error) {
+        console.log(error);
+        res.send('An error occurred while finding the cadet.');
+    }
+}
+
