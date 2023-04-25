1b4a714cd229d4b127a09d489d470aa3cca7c728
Vrund87 <vrundrajput2002@gmail.com>
2023-04-25 11:26:57 +0530

    updated authRoutes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,30 +1,33 @@
 const { Router } = require('express');
-const authController = require('../controllers/authController');
 const router = Router();
+const authController = require('../controllers/authController');
+const methodoverride = require('method-override');
 const { requireAuth, checkUser } = require('../middleware/authMiddleware');
+router.use(methodoverride('_method'));
+
+router.get('*', checkUser);
+router.patch('*', checkUser);
+router.post('*', checkUser);
 
 router.get('/', (req, res) => {
     res.render('home', { title: 'Home' });
 });
 
-
-router.get('*', checkUser);
-router.get('/signup', authController.signup_get);
-router.post('/signup', authController.signup_post);
+router.get('/home', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
 
 router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
 
-router.get('/secret', requireAuth, authController.secret_get);
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
 
 router.get('/logout', authController.logout_get);
 
-router.get('/customer/:username/about', authController.customer_about_get);
-
-router.get('/cadet/:username/about', authController.cadet_about_get);
-
-router.get('/manager/:username/about', authController.manager_about_get);
-
-router.get('/logout', authController.logout_get);
+router.use((req, res) => {
+    res.status(404);
+    res.render('404', { title: '404' });
+});
 
 module.exports = router;
